7ba3986fd88d97db8a054b4e12a6ed73464b03ba
Ronan Bastos <ronanbastos@hotmail.com>
2019-11-26 16:58:13 -0300

    Update Jframe.java

--- JarnanScript/src/com/script/jarnan/java/Jframe.java
+++ JarnanScript/src/com/script/jarnan/java/Jframe.java
@@ -1,29 +1,73 @@
 package com.data.java;
 
+import java.awt.List;
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.Reader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
 import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
 import com.data.visao.DrawLines;
+import com.opencsv.CSVReader;
+import com.opencsv.CSVReaderBuilder;
 import com.script.jarnan.Jarnan;
 
+@SuppressWarnings("unused")
 public class Jframe {
 
 	public static void main(String[] args) {
-		
-		 Jarnan j = new Jarnan();
-	       
-         j.setVisible(true);
-          
-		
-		DrawLines panel = new DrawLines();
-		
-	 
-	 	JFrame frame= new JFrame("Jarnan DataScience 1.0v");
 
-        frame.setDefaultCloseOperation( JFrame.EXIT_ON_CLOSE );
-        frame.setSize( 250, 250 );
-        frame.setVisible( true);
-        frame.add( panel );
-    	frame.setLocation(50,10);
-	       
+		Jarnan j = new Jarnan();
+		j.setVisible(true);
+
+		// Jframe obj = new Jframe();
+		// obj.run();
+
 	}
 
+	public void run() {
+		/*
+		 * try { Reader reader =
+		 * Files.newBufferedReader(Paths.get("C:\\Users\\2025\\Desktop\\notas.csv"));
+		 * CSVReader csvReader = new CSVReaderBuilder(reader) .withSkipLines(1)//para o
+		 * caso do CSV ter cabeçalho. .build();
+		 * 
+		 * java.util.List<String[]> linhas = csvReader.readAll(); for (String[] linha :
+		 * linhas) for (String coluna : linha) System.out.println(coluna);
+		 * 
+		 * }
+		 * 
+		 * catch(IOException e) { e.printStackTrace(); }
+		 * 
+		 * 
+		 * String arquivoCSV = "C:\\Users\\2025\\Desktop\\notas.csv"; BufferedReader br
+		 * = null; String linha = ""; String csvDivisor = ","; try {
+		 * 
+		 * br = new BufferedReader(new FileReader(arquivoCSV)); while ((linha =
+		 * br.readLine()) != null) {
+		 * 
+		 * String[] dados = linha.split(csvDivisor); /* for(String x : dados) {
+		 * System.out.println(x); }
+		 * 
+		 * 
+		 * System.out.println(dados[dados.length-1].toString());
+		 * 
+		 * }
+		 * 
+		 * } catch (FileNotFoundException e) { e.printStackTrace(); } catch (IOException
+		 * e) { e.printStackTrace(); } finally { if (br != null) { try { br.close(); }
+		 * catch (IOException e) { e.printStackTrace(); } } }
+		 * 
+		 * 
+		 * 
+		 * 
+		 * }
+		 */
+	}
 }

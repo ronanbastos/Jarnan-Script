298810105bf85289c1fec5328f6715a1ae927f1b
Ronan Bastos <ronanbastos@hotmail.com>
2019-11-27 02:59:11 -0300

    Update Jarnan.java
    att 1.5v   
    Ultima att 27/11/2019

--- JarnanScript/src/com/script/jarnan/Jarnan.java
+++ JarnanScript/src/com/script/jarnan/Jarnan.java
@@ -1,23 +1,35 @@
 package com.script.jarnan;
 
+import java.awt.Color;
 /**
  * @author Ronan Bastos (https://github.com/ronanbastos)
  */
 import java.awt.FlowLayout;
 import java.awt.Font;
+import java.awt.Graphics2D;
 import java.awt.TextArea;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+
+import javax.swing.ButtonGroup;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JCheckBoxMenuItem;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
 import javax.swing.JPanel;
+import javax.swing.JRadioButtonMenuItem;
 import javax.swing.JScrollPane;
 import javax.swing.JTextField;
 import com.data.visao.DrawLines;
 
-public class Jarnan extends JFrame {
+@SuppressWarnings("unused")
+public class Jarnan extends JFrame implements Runnable {
 	/**
 	* 
 	*/
@@ -28,9 +40,13 @@ public class Jarnan extends JFrame {
 	JTextField text;
 	JScrollPane scroll;
 	JLabel label;
+	JLabel labeln;
+	JLabel labelnn;
+	JLabel labelnnn;
+	JLabel labelc;
 	public String str;
 	public int[] strInt;
-
+	public Graphics2D g2d;
 	TextArea txt_AreaCodigo = new TextArea(10, 70);
 	TextArea txt_AreaResultado = new TextArea(10, 70);
 
@@ -38,99 +54,95 @@ public class Jarnan extends JFrame {
 	public ArrayList<String> getvariavel = new ArrayList<String>();
 
 	public Jarnan() {
-		super("Jarnan Script-1.0v");
-
-		setSize(400, 300);
-		setLocationRelativeTo(null);
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-
+		super("Jarnan Script-1.5v");
 		Adiciona();
 		setContentPane(painel);
-		this.setLocation(50, 260);
-
+		setLocation(380,150);
 		pack();
 	}
 
 	public void Adiciona() {
 
+		ImageIcon imgbut = new ImageIcon("C:\\Java\\DataScience\\Utili\\btn_circled.png");
+		ImageIcon imgcons = new ImageIcon("C:\\Java\\DataScience\\Utili\\console.png");
+		b = new JButton(imgbut);
 		painel = new JPanel();
-		painel.add(txt_AreaCodigo);
-		b = new JButton("Run");
-		b.setBounds(100, 150, 150, 100);
-		b.setFont(new Font("Verdana", Font.BOLD, 14));
 		painel.add(b);
-		label = new JLabel("Console:");
+		label = new JLabel(imgcons);
+		labelc = new JLabel("Console");
+		labeln = new JLabel("Jarnan Script ()");
+		labelnn = new JLabel("}");
+		labelnnn = new JLabel("{");
+		JMenuBar menuBar = new JMenuBar();
+		setJMenuBar(menuBar);
+		JMenu fileMenu = new JMenu("File");
+		JMenu editMenu = new JMenu("Edit");
+		JMenu helpMenu = new JMenu("Config");
+		menuBar.add(fileMenu);
+		menuBar.add(editMenu);
+		menuBar.add(helpMenu);
+		JMenuItem newAction = new JMenuItem("Novo");
+		JMenuItem openAction = new JMenuItem("Abrir...");
+		JMenuItem saveAction = new JMenuItem("Salva");
+		JMenuItem saveasAction = new JMenuItem("Salvar como...");
+		JMenuItem helpAction = new JMenuItem("Help");
+		JMenuItem sobreAction = new JMenuItem("Sobre");
+		JMenuItem ajustaAction = new JMenuItem("Ajustar");
+		JMenuItem cutAction = new JMenuItem("Cut");
+		JMenuItem copyAction = new JMenuItem("Copy");
+		JMenuItem pasteAction = new JMenuItem("Paste");
+		fileMenu.add(newAction);
+		fileMenu.add(openAction);
+		fileMenu.add(saveAction);
+		fileMenu.add(saveasAction);
+		helpMenu.add(helpAction);
+		helpMenu.add(sobreAction);
+		helpMenu.add(ajustaAction);
+		editMenu.add(cutAction);
+		editMenu.add(copyAction);
+		editMenu.add(pasteAction);
 		painel.add(label);
+		painel.add(labelc);
+		painel.add(labeln);
+		painel.add(labelnn);
+		painel.add(labelnnn);
+		painel.add(txt_AreaCodigo);
 		painel.add(txt_AreaResultado);
 		painel.setVisible(true);
 		painel.setFocusable(false);
 		painel.setLayout(null);
-		painel.setBounds(10, 10, 10, 10);
-		painel.setLayout(new FlowLayout());
-		b.setLayout(new FlowLayout());
+		painel.setBounds(100, 100, 100, 100);
+		// painel.setLayout(new FlowLayout());
+		// b.setLayout(new FlowLayout());
+		label.setBounds(40, 235, 50, 15);
+		labeln.setBounds(60,15, 150, 15);
+		labelnn.setBounds(65,200, 150, 15);
+		labelnnn.setBounds(65,30, 150, 15);
+		labelc.setBounds(75, 240, 50, 10);
+		b.setBounds(630, 120, 19, 15);
+		txt_AreaCodigo.setBounds(60, 50, 550, 150);
+		txt_AreaResultado.setBounds(60, 250, 550, 150);
 		txt_AreaCodigo.setFocusable(true);
 		txt_AreaCodigo.setCaretPosition(txt_AreaCodigo.getText().length());
 		txt_AreaResultado.setEditable(false);
-		txt_AreaResultado.setFocusable(true);
 		txt_AreaResultado.setCaretPosition(txt_AreaResultado.getText().length());
-
+		txt_AreaResultado.setRows(1);
 		b.addActionListener(new ActionListener() {
 
 			public void actionPerformed(ActionEvent e) {
 
-				String str1 = "pr:";
-				str = txt_AreaCodigo.getText();
-
-				if (str.contains(str1)) {
-
-					print();
-
-				}
-
-				String str2 = ":";
-				str = txt_AreaCodigo.getText();
-				if (str.contains(str2)) {
-
-					SetVariavel();
-
-				}
-				String str3 = "clear";
-				if (str.contains(str3)) {
-
-					txt_AreaResultado.setText(" ");
-
-				}
-				String str4 = "v:";
-
-				if (str.contains(str4)) {
-					String[] s = str.split("v:");
-					String x = s[1];
-					str = x;
-
-					GetVariavel();
-				}
-				String str5 = "for";
-
-				if (str.contains(str5)) {
-
-					AtivaFor();
-				}
-				String str6 = "g";
-
-				if (str.contains(str6)) {
-
-					DrawLines grafico = new DrawLines();
-					grafico.valores();
-					JFrame frame = new JFrame("Jarnan DataScience 1.0v");
-					frame.setVisible(true);
-					frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-					frame.setSize(250, 250);
-					frame.add(grafico);
-					frame.setLocation(50, 10);
-
-				}
+				run();
 
 			}
+		});
+		
+		newAction.addActionListener(new ActionListener() {
+		    public void actionPerformed(ActionEvent ae) {
+		    	Jarnan j = new Jarnan();
+				j.setVisible(true);
+				j.setSize(685,455);
+				
+		    }
 		});
 
 	}
@@ -150,7 +162,7 @@ public class Jarnan extends JFrame {
 		String[] s = str.split(":");
 
 		setvariavel.add(new String(s[0]));
-		System.out.print(s[0]);
+		System.out.println(s[0]);
 		getvariavel.add(new String(s[1]));
 		System.out.print(s[1]);
 
@@ -158,7 +170,7 @@ public class Jarnan extends JFrame {
 
 	public void GetVariavel() {
 
-		for (int i = 0; i <= 100; i++) {
+		for (int i = 0; i <= setvariavel.size(); i++) {
 
 			if (str.trim().equals(setvariavel.get(i).toString())) {
 				txt_AreaResultado.setText(" ");
@@ -194,6 +206,64 @@ public class Jarnan extends JFrame {
 		for (int i = x1; i <= x2; i++) {
 
 			txt_AreaResultado.setText(txt_AreaResultado.getText() + "For:" + i + "\n");
+		}
+
+	}
+
+	@Override
+	public void run() {
+
+		String str1 = "pr:";
+		str = txt_AreaCodigo.getText();
+
+		if (str.contains(str1)) {
+
+			print();
+
+		}
+
+		String str2 = ":";
+		str = txt_AreaCodigo.getText();
+		if (str.contains(str2)) {
+
+			SetVariavel();
+
+		}
+		String str3 = "clear";
+		if (str.contains(str3)) {
+
+			txt_AreaResultado.setText(" ");
+
+		}
+		String str4 = "v->";
+
+		if (str.contains(str4)) {
+			String[] s = str.split("v->");
+
+			String x = s[1];
+			str = x;
+
+			GetVariavel();
+		}
+		String str5 = "for";
+
+		if (str.contains(str5)) {
+
+			AtivaFor();
+		}
+		String str6 = "g";
+
+		if (str.contains(str6)) {
+
+			DrawLines grafico = new DrawLines();
+			grafico.valores();
+			JFrame frame = new JFrame("Jarnan DataScience 1.0v");
+			frame.setVisible(true);
+			frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+			frame.setSize(250, 250);
+			frame.add(grafico);
+			frame.setLocation(50, 10);
+
 		}
 
 	}

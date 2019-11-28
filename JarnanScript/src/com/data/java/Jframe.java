0be14ac6a2f6913c5679170eb770ca7625b22a2b
Ronan Bastos <ronanbastos@hotmail.com>
2019-11-27 21:47:04 -0300

    Add files via upload
    !bkp
    !data 27/11/2019

---
+++ JarnanScript/src/com/data/java/Jframe.java
@@ -0,0 +1,31 @@
+package com.data.java;
+
+
+
+import java.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+
+import com.script.jarnan.Jarnan;
+
+@SuppressWarnings("unused")
+public class Jframe {
+
+	public static void main(String[] args) {
+
+		Jarnan j = new Jarnan();
+		j.setVisible(true);
+		j.setSize(685,455);
+		j.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		
+
+	}
+
+	public void run() {
+		
+	}
+}
